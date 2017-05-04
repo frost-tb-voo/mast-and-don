7322e7d34e22d4427099aab6c6cd0e38009f97c4
Novi Yama <aida9devflex@gmail.com>
2017-05-05 07:42:34 +0900

    Add notification badges.

--- src/content_scripts/content-script.js
+++ src/content_scripts/content-script.js
@@ -1,5 +1,5 @@
 console.log('load content-script.');
-//var browser = chrome;
+var browser = chrome;
 
 function onReceived(message) {
   if (!message) {
@@ -8,42 +8,29 @@ function onReceived(message) {
   // console.log(JSON.stringify(message, null, 2));
 }
 
+
+
 function onFailed(err) {
-  console.error(err);
+  if (err) {
+    console.error(err);
+  }
 }
 
 function loadFromBackground(e) {
   if (!e.type)
     return;
+  authorize();
+  checkConfirmedHost();
+}
 
-  // console.log("content script sending message " + e.type);
-  var location = window.location;
-  var hostname = new URL(location).hostname;
-  // console.log('hostname : ' + hostname);
-
-  var code = getCode();
-  if (!code) {
-    return;
-  }
-  if (location != 'https://' + hostname + '/oauth/authorize/' + code) {
-    return;
-  }
-  // console.log('returned code : ' + code);
+function sendMessage(requestMessage) {
   if (browser != chrome) {
-    var sending = browser.runtime.sendMessage({
-      content: true,
-      hostname: hostname,
-      code: code
-    });
+    var sending = browser.runtime.sendMessage(requestMessage);
     sending.then(onReceived, onFailed);
   } else {
-    browser.runtime.sendMessage({
-      content: true,
-      hostname: hostname,
-      code: code
-    }, function (message) {
-      if (message) {
-        onReceived(message);
+    browser.runtime.sendMessage(requestMessage, function (responseMessage) {
+      if (responseMessage) {
+        onReceived(responseMessage);
       } else {
         if (browser.runtime) {
           onFailed(browser.runtime.lastError);
@@ -53,10 +40,76 @@ function loadFromBackground(e) {
   }
 }
 
+function authorize() {
+  var location = window.location;
+  var hostname = new URL(location).hostname;
+
+  var code = getCode();
+  if (!code) {
+    return;
+  }
+  if (location != 'https://' + hostname + '/oauth/authorize/' + code) {
+    return;
+  }
+  // console.log("content script sending message " + e.type);
+  sendMessage({
+    content: true,
+    hostname: hostname,
+    code: code
+  });
+}
+
+function checkConfirmedHost() {
+  var location = window.location;
+  var hostname = new URL(location).hostname;
+  if (location == 'https://' + hostname + '/web/getting_started') {
+    sendMessage({
+      confirm: true,
+      hostname: hostname,
+      page: '/web/getting_started'
+    });
+  }
+  if (location == 'https://' + hostname + '/web/follow_requests') {
+    sendMessage({
+      confirm: true,
+      hostname: hostname,
+      page: '/web/follow_requests'
+    });
+  }
+  if (location == 'https://' + hostname + '/web/notifications') {
+    sendMessage({
+      confirm: true,
+      hostname: hostname,
+      page: '/web/notifications'
+    });
+  }
+  if (location == 'https://' + hostname + '/web/timelines/home') {
+    sendMessage({
+      confirm: true,
+      hostname: hostname,
+      page: '/web/timelines/home'
+    });
+  }
+  if (location == 'https://' + hostname + '/web/timelines/public') {
+    sendMessage({
+      confirm: true,
+      hostname: hostname,
+      page: '/web/timelines/public'
+    });
+  }
+  if (location == 'https://' + hostname + '/web/timelines/public/local') {
+    sendMessage({
+      confirm: true,
+      hostname: hostname,
+      page: '/web/timelines/public/local'
+    });
+  }
+}
+
 function getCode() {
   for (var code of document.getElementsByTagName("code")) {
     var code = code.innerText;
-    
+
     // console.log('returned code : ' + code);
     return code;
   }
